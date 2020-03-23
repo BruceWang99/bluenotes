b5a223960093ce1502d6dcefb5b496fd10f6e050
wangxiaoming8989 <wangxiaoming8989@sina.com>
2020-03-23 12:50:14 +0800

    fix

--- config/config.default.js
+++ config/config.default.js
@@ -56,12 +56,14 @@ module.exports = appInfo => {
 
   config.sequelize = {
     dialect: 'mysql',
-    host: '112.124.16.145',
+    host: '127.0.0.1',
     port: 3306,
-    database: 'bluenote_pro',
-    username: 'bluenote_pro',
-    password: 'Ww:123456',
+    database: 'bluenote_dev',
+    username: 'root',
+    password: '123456',
+    // 是否自动进行下划线转换（这里是因为DB默认的命名规则是下划线方式，而我们使用的大多数是驼峰方式）
     underscored: true,
+    // 时区，sequelize有很多自动时间的方法，都是和时区相关的，记得设置成东8区（+08:00）
     timezone: '+08:00',
   };
 
