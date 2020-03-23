68a87352a6bb5e9ddaddd8df362cbc16ff81f47f
wangxiaoming8989 <wangxiaoming8989@sina.com>
2020-03-23 11:42:53 +0800

    fix config

--- config/config.production.js
+++ config/config.production.js
@@ -18,6 +18,8 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  // 引入Op模块
+  const Op = require('sequelize').Op;
   config.sequelize = {
     dialect: 'mysql',
     host: '112.124.16.145',
@@ -29,6 +31,43 @@ module.exports = appInfo => {
     underscored: true,
     // 时区，sequelize有很多自动时间的方法，都是和时区相关的，记得设置成东8区（+08:00）
     timezone: '+08:00',
+    operatorsAliases: {
+      $eq: Op.eq,
+      $ne: Op.ne,
+      $gte: Op.gte,
+      $gt: Op.gt,
+      $lte: Op.lte,
+      $lt: Op.lt,
+      $not: Op.not,
+      $in: Op.in,
+      $notIn: Op.notIn,
+      $is: Op.is,
+      $like: Op.like,
+      $notLike: Op.notLike,
+      $iLike: Op.iLike,
+      $notILike: Op.notILike,
+      $regexp: Op.regexp,
+      $notRegexp: Op.notRegexp,
+      $iRegexp: Op.iRegexp,
+      $notIRegexp: Op.notIRegexp,
+      $between: Op.between,
+      $notBetween: Op.notBetween,
+      $overlap: Op.overlap,
+      $contains: Op.contains,
+      $contained: Op.contained,
+      $adjacent: Op.adjacent,
+      $strictLeft: Op.strictLeft,
+      $strictRight: Op.strictRight,
+      $noExtendRight: Op.noExtendRight,
+      $noExtendLeft: Op.noExtendLeft,
+      $and: Op.and,
+      $or: Op.or,
+      $any: Op.any,
+      $all: Op.all,
+      $values: Op.values,
+      $col: Op.col,
+      $fn: Op.fn,
+    },
   };
 
   return {
